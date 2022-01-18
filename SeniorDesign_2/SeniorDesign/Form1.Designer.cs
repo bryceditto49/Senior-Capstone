b7ae16032f383dbc5c6c40ab8cfae12c288775ac
varat2542 <91092070+varat2542@users.noreply.github.com>
2022-01-18 07:50:46 -0600

    Push Form 1-6 without hiding function

---
+++ SeniorDesign_2/SeniorDesign/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace SeniorDesign
+{
+    partial class StartFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.nextbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Louisiana",
+            "Texas",
+            "Arkansas",
+            "Alabama",
+            "Mississippi"});
+            this.comboBox1.Location = new System.Drawing.Point(302, 190);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(193, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Please Enter A State";
+            // 
+            // nextbutton
+            // 
+            this.nextbutton.Location = new System.Drawing.Point(357, 230);
+            this.nextbutton.Name = "nextbutton";
+            this.nextbutton.Size = new System.Drawing.Size(75, 23);
+            this.nextbutton.TabIndex = 1;
+            this.nextbutton.Text = "Submit";
+            this.nextbutton.UseVisualStyleBackColor = true;
+            this.nextbutton.Click += new System.EventHandler(this.nextbutton_Click);
+            // 
+            // StartFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nextbutton);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "StartFrm";
+            this.Text = "Working Title";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button nextbutton;
+    }
+}
+
---
+++ SeniorDesign_2/SeniorDesign/Form3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeniorDesign
+{
+    public partial class DataSelectionFrm : Form
+    {
+        public DataSelectionFrm()
+        {
+            InitializeComponent();
+        }
+        private void covidbutton_Click(object sender, EventArgs e)
+        {
+            CovidDataFrm form4 = new CovidDataFrm();
+            form4.Show();
+        }
+ 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FBIdatafrm form5 = new FBIdatafrm();
+            form5.Show();
+        }
+       private void button1_Click(object sender, EventArgs e)
+        {
+        
+            
+        }
+
+        private void terrordatabutton_Click(object sender, EventArgs e)
+        {
+            TerrorismDataFrm form6 = new TerrorismDataFrm();
+            form6.Show();
+        }
+    }
+}
