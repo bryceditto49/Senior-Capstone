decd1d1f700e9745ac3c0f4f498c842d44b11129
SerperiorSoul <ltwebb42@gmail.com>
2022-02-16 15:07:31 -0600

    added a text box and got th backgroun configured

--- NewOrderDesign/FBIUSView.Designer.cs
+++ NewOrderDesign/FBIUSView.Designer.cs
@@ -30,6 +30,7 @@ namespace NewOrderDesign
         private void InitializeComponent()
         {
             this.continueFBIUSView = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // continueFBIUSView
@@ -40,21 +41,38 @@ namespace NewOrderDesign
             this.continueFBIUSView.TabIndex = 0;
             this.continueFBIUSView.Text = "Continue";
             this.continueFBIUSView.UseVisualStyleBackColor = true;
+            this.continueFBIUSView.Click += new System.EventHandler(this.continueFBIUSView_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(253, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 37);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "US Overview";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // FBIUSView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::NewOrderDesign.Properties.Resources.background1;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.continueFBIUSView);
             this.Name = "FBIUSView";
             this.Text = "Form5";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button continueFBIUSView;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
