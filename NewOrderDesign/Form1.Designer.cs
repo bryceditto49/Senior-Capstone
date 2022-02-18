5e0053bbf4f37c11e0bba4ddd064ae002f45d7ce
varat2542 <91092070+varat2542@users.noreply.github.com>
2022-02-18 08:47:32 -0600

    new app with login

--- NewOrderDesign/Form1.Designer.cs
+++ NewOrderDesign/Form1.Designer.cs
@@ -30,71 +30,102 @@ namespace NewOrderDesign
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.nextbutton = new System.Windows.Forms.Button();
-            this.registerlink = new System.Windows.Forms.LinkLabel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btsubmit = new System.Windows.Forms.Button();
+            this.btguess = new System.Windows.Forms.Button();
+            this.lbuser = new System.Windows.Forms.Label();
+            this.lbpass = new System.Windows.Forms.Label();
+            this.lbfirst = new System.Windows.Forms.Label();
+            this.txtcon = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtuser
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(448, 53);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(215, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtuser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtuser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.txtuser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtuser.Location = new System.Drawing.Point(151, 32);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(215, 20);
+            this.txtuser.TabIndex = 0;
+            this.txtuser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // txtpass
             // 
-            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(448, 101);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(215, 20);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.txtpass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.txtpass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtpass.Location = new System.Drawing.Point(151, 78);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(215, 20);
+            this.txtpass.TabIndex = 1;
+            this.txtpass.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // nextbutton
+            // btsubmit
             // 
-            this.nextbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
-            this.nextbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.nextbutton.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.nextbutton.Location = new System.Drawing.Point(588, 141);
-            this.nextbutton.Name = "nextbutton";
-            this.nextbutton.Size = new System.Drawing.Size(75, 23);
-            this.nextbutton.TabIndex = 2;
-            this.nextbutton.Text = "Submit";
-            this.nextbutton.UseVisualStyleBackColor = false;
-            this.nextbutton.Click += new System.EventHandler(this.nextbutton_Click);
+            this.btsubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.btsubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btsubmit.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btsubmit.Location = new System.Drawing.Point(151, 181);
+            this.btsubmit.Name = "btsubmit";
+            this.btsubmit.Size = new System.Drawing.Size(75, 23);
+            this.btsubmit.TabIndex = 2;
+            this.btsubmit.Text = "Register";
+            this.btsubmit.UseVisualStyleBackColor = false;
+            this.btsubmit.Click += new System.EventHandler(this.btsubmit_Click);
             // 
-            // registerlink
+            // btguess
             // 
-            this.registerlink.AutoSize = true;
-            this.registerlink.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
-            this.registerlink.DisabledLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
-            this.registerlink.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.registerlink.Location = new System.Drawing.Point(448, 150);
-            this.registerlink.Name = "registerlink";
-            this.registerlink.Size = new System.Drawing.Size(46, 13);
-            this.registerlink.TabIndex = 3;
-            this.registerlink.TabStop = true;
-            this.registerlink.Text = "Register";
-            this.registerlink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.registerlink_LinkClicked);
+            this.btguess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.btguess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btguess.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btguess.Location = new System.Drawing.Point(291, 181);
+            this.btguess.Name = "btguess";
+            this.btguess.Size = new System.Drawing.Size(75, 23);
+            this.btguess.TabIndex = 7;
+            this.btguess.Text = "Guest Login";
+            this.btguess.UseVisualStyleBackColor = false;
+            this.btguess.Click += new System.EventHandler(this.btguess_Click);
             // 
-            // label1
+            // lbuser
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(294, 86);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Working Title";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lbuser.AutoSize = true;
+            this.lbuser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.lbuser.Location = new System.Drawing.Point(35, 34);
+            this.lbuser.Name = "lbuser";
+            this.lbuser.Size = new System.Drawing.Size(83, 13);
+            this.lbuser.TabIndex = 4;
+            this.lbuser.Text = "Enter Username";
+            this.lbuser.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbpass
+            // 
+            this.lbpass.AutoSize = true;
+            this.lbpass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.lbpass.Location = new System.Drawing.Point(35, 80);
+            this.lbpass.Name = "lbpass";
+            this.lbpass.Size = new System.Drawing.Size(81, 13);
+            this.lbpass.TabIndex = 8;
+            this.lbpass.Text = "Enter Password";
+            // 
+            // lbfirst
+            // 
+            this.lbfirst.AutoSize = true;
+            this.lbfirst.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.lbfirst.Location = new System.Drawing.Point(35, 127);
+            this.lbfirst.Name = "lbfirst";
+            this.lbfirst.Size = new System.Drawing.Size(91, 13);
+            this.lbfirst.TabIndex = 9;
+            this.lbfirst.Text = "Confirm Password";
+            // 
+            // txtcon
+            // 
+            this.txtcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.txtcon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtcon.Location = new System.Drawing.Point(151, 125);
+            this.txtcon.Name = "txtcon";
+            this.txtcon.Size = new System.Drawing.Size(215, 20);
+            this.txtcon.TabIndex = 5;
             // 
             // Form1
             // 
@@ -102,15 +133,18 @@ namespace NewOrderDesign
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::NewOrderDesign.Properties.Resources.background1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.registerlink);
-            this.Controls.Add(this.nextbutton);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(475, 313);
+            this.Controls.Add(this.lbfirst);
+            this.Controls.Add(this.lbpass);
+            this.Controls.Add(this.btguess);
+            this.Controls.Add(this.txtcon);
+            this.Controls.Add(this.lbuser);
+            this.Controls.Add(this.btsubmit);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Working Title";
+            this.Text = "TravelSite";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -119,11 +153,14 @@ namespace NewOrderDesign
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button nextbutton;
-        private System.Windows.Forms.LinkLabel registerlink;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button btsubmit;
+        private System.Windows.Forms.Button btguess;
+        private System.Windows.Forms.Label lbuser;
+        private System.Windows.Forms.Label lbpass;
+        private System.Windows.Forms.Label lbfirst;
+        private System.Windows.Forms.TextBox txtcon;
     }
 }
 
--- NewOrderDesign/Form1.cs
+++ NewOrderDesign/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace NewOrderDesign
 {
     public partial class Form1 : Form
     {
+        SqlCommand cmd;
+        SqlConnection cn;
+        SqlDataReader dr;
+        
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +24,8 @@ namespace NewOrderDesign
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\varat\source\repos\bryceditto49\Senior-Capstone\NewOrderDesign\Database1.mdf;Integrated Security=True");
+            cn.Open();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -26,28 +33,67 @@ namespace NewOrderDesign
 
         }
 
-        private void nextbutton_Click(object sender, EventArgs e)
+        private void btsubmit_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            Form2 form2 = new Form2();
-            form2.Show();
+            if (txtcon.Text != string.Empty || txtpass.Text != string.Empty || txtuser.Text != string.Empty)
+            {
+                if (txtpass.Text == txtcon.Text)
+                {
+                    cmd = new SqlCommand("select * from Table where username='" + txtuser.Text + "'", cn);
+                    dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        dr.Close();
+                        MessageBox.Show("Username Already exist please try another ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        dr.Close();
+                        cmd = new SqlCommand("insert into Table values(@username,@password)", cn);
+                        cmd.Parameters.AddWithValue("username", txtuser.Text);
+                        cmd.Parameters.AddWithValue("password", txtpass.Text);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Account Created.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Password missmatch", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter value in all field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         string username;
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            username = textBox1.Text;
+            username = txtuser.Text;
         }
 
         string password;
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            password = textBox2.Text;
+            password = txtpass.Text;
         }
 
-        private void registerlink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void btsubmit_Clicked(object sender, EventArgs e)
         {
 
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btguess_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 form2 = new Form2();
+            form2.Show();
         }
     }
 }
